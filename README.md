473028c9e909bb32d7b5044a12987f74828cdc59
Dion Timmer <diontimmer@live.nl>
2023-04-17 21:45:24 -0400

    full folder support

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 
 # AI Audio Upsampling
 
-Restoration/Variation of audio using chunked diffusion. Heavily based on Reed+Sam AI's SplitDiffusion implementation of Zach Evans Dance Diffusion. Uses dance diffusion to restore audio in variable chunks.
+Restoration/Variation of audio using chunked diffusion. Heavily based on [Reed+Sam AI's SplitDiffusion]("https://colab.research.google.com/drive/1fkwG7yzy9uegEoMpigKNpA0pmXyzrBAz#scrollTo=--Ml82LICtki") implementation of Zach Evans Dance Diffusion. Uses dance diffusion to restore audio in variable chunks. Big shoutout to Reed+Sam Media for coming up with the original idea, it works really great and its fun to mess with.
+
+This is basically a CLI wrapper for the Reed+Sam notebook so you can whip it out and use it on a bunch of files fast.
 
 
 ## Installation
@@ -27,9 +29,9 @@ pip install -r requirements.txt
 ```
 python cli.py ->
   -i INPUT, --input INPUT
-                        Input file to upsample/restore (input.wav)
+                        Input folder to upsample/restore
   -o OUTPUT, --output OUTPUT
-                        Output file to render (output.wav)
+                        Output folder to render to
   -c CKPT, --ckpt CKPT  Checkpoint Path (Upsampler/DD model)
   -s SAMPLER, --sampler SAMPLER
                         Sampler type ([)fast, hq, normal, adaptive])
--- cli.py
+++ cli.py
@@ -18,20 +18,20 @@ import argparse
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
-def upscale_audio(input_file, output_file, ckpt_path='rpsmai_diffusion_musdb18hq_ins_v1.ckpt', sampler='adaptive', steps=12, noise_level=0.6, chunk_length=131072, sample_rate=44100):
-
+def upscale_audio(input_folder, output_folder, ckpt_path='rpsmai_diffusion_23pop_ins_v1.ckpt', sampler='adaptive', steps=12, noise_level=0.6, chunk_length=131072, sample_rate=44100):
     # Configurable models and cache folders.
-
     modelsfolder = os.path.realpath('models/')
     workingpath = os.path.realpath('cache/')
-
     if not os.path.dirname(ckpt_path):
         ckpt_path = os.path.join(modelsfolder, ckpt_path)
-
+    if not os.path.exists(ckpt_path):
+      print('Please download a DD model ckpt file and try again.')
+      exit()
+    if not os.path.exists(workingpath):
+        os.makedirs(workingpath)
     # Create sampler
-
-    sampler_type = get_sampler(sampler) #fast hq normal adaptive
-    
+    model_fn = create_model(ckpt_path, sample_rate=sample_rate, sample_size=chunk_length)
+    sampler_type = get_sampler(sampler) #fast hq normal adaptive 
     #Create Processing File
     
     proc_folder = os.path.join(workingpath, "no_vocals_"+sampler_type+"_output/")
@@ -40,60 +40,63 @@ def upscale_audio(input_file, output_file, ckpt_path='rpsmai_diffusion_musdb18hq
     else:
       for filename in os.listdir(proc_folder):
         os.remove(os.path.join(proc_folder, filename))
-    
-    myaudio = AudioSegment.from_file(input_file, "wav") 
-    chunk_length_ms = ((chunk_length) / sample_rate) *1000 * 1 # pydub calculates in millisec 
-    chunks = make_chunks(myaudio,chunk_length_ms) #Make chunks of 65535 samples 
-    for i, chunk in enumerate(chunks): 
-        chunk_name = os.path.join(proc_folder, f'{i}.wav')
-        print ("exporting", chunk_name) 
-        chunk.export(chunk_name, format="wav")
 
-    # create model fn
-    model_fn = create_model(ckpt_path, sample_rate=sample_rate, sample_size=chunk_length)
-    
-    for filename in sorted(os.listdir(proc_folder)):
-      if not filename.endswith("_upsc.wav") and not filename.endswith("_upsc-final.wav") and not filename.endswith("_upsc-final2.wav") and not filename.endswith("item_0.wav"):
-        f = os.path.join(proc_folder, filename)
-        if os.path.isfile(f):
-          torch.cuda.empty_cache()
-          gc.collect()
-        
-          augs = torch.nn.Sequential(
-            PadCrop(chunk_length, randomize=True),
-            Stereo()
-          )
-        
-          fp = f
-          print("Diffusing Chunk "+fp)
-          audio_sample = load_to_device(fp, sample_rate, device)
-          audio_sample = augs(audio_sample).unsqueeze(0).repeat([1, 1, 1])
-          generated = resample(model_fn, audio_sample, steps, sampler_type, noise_level=noise_level, chunk_length=chunk_length)
-        
-          print("Saving")
-          save_upsc_audio(rearrange(generated, 'b d n -> d (b n)'), fp+"_upsc.wav", sample_rate)
-          print("Done")
-    
-    finalupscoutput = AudioSegment.empty()
-    for filename in sorted((os.listdir(proc_folder))):
-      if filename.endswith("_upsc.wav"):
-        f = os.path.join(proc_folder, filename)
-        if os.path.isfile(f):
-          finalupscoutput = finalupscoutput.append(AudioSegment.from_file(f, format="wav"), crossfade=0 if not len(finalupscoutput) else 10)
 
-    for filename in os.listdir(proc_folder):
-      os.remove(os.path.join(proc_folder, filename))
+        # loop folder
 
-    
-    finalupscoutput.export(output_file, format="wav")
-    return finalupscoutput
+    for i, input_file in enumerate(os.listdir(input_folder)):
+      input_file = os.path.join(input_folder, input_file)
+      print(f'Upsampling {input_file}... ({i+1}/{len(os.listdir(input_folder))})')
+      myaudio = AudioSegment.from_file(input_file, "wav") 
+      chunk_length_ms = ((chunk_length) / sample_rate) *1000 * 1 # pydub calculates in millisec 
+      chunks = make_chunks(myaudio,chunk_length_ms) #Make chunks of 65535 samples
+      print(f'Made {len(chunks)} chunks of {chunk_length_ms} ms each')
+      for i, chunk in enumerate(chunks): 
+          chunk_name = os.path.join(proc_folder, f'{i}.wav')
+          print ("exporting", chunk_name) 
+          chunk.export(chunk_name, format="wav")
+      
+      for filename in sorted(os.listdir(proc_folder)):
+        if not filename.endswith("_upsc.wav") and not filename.endswith("_upsc-final.wav") and not filename.endswith("_upsc-final2.wav") and not filename.endswith("item_0.wav"):
+          f = os.path.join(proc_folder, filename)
+          if os.path.isfile(f):
+            torch.cuda.empty_cache()
+            gc.collect()
+          
+            augs = torch.nn.Sequential(
+              PadCrop(chunk_length, randomize=True),
+              Stereo()
+            )
+          
+            fp = f
+            print("Diffusing Chunk "+fp)
+            audio_sample = load_to_device(fp, sample_rate, device)
+            audio_sample = augs(audio_sample).unsqueeze(0).repeat([1, 1, 1])
+            generated = resample(model_fn, audio_sample, steps, sampler_type, noise_level=noise_level, chunk_length=chunk_length)
+          
+            print("Saving")
+            save_upsc_audio(rearrange(generated, 'b d n -> d (b n)'), fp+"_upsc.wav", sample_rate)
+            print("Done")
+      
+      finalupscoutput = AudioSegment.empty()
+      for filename in sorted((os.listdir(proc_folder))):
+        if filename.endswith("_upsc.wav"):
+          f = os.path.join(proc_folder, filename)
+          if os.path.isfile(f):
+            finalupscoutput = finalupscoutput.append(AudioSegment.from_file(f, format="wav"), crossfade=0 if not len(finalupscoutput) else 10)
+  
+      for filename in os.listdir(proc_folder):
+        os.remove(os.path.join(proc_folder, filename))
+  
+      output_file = os.path.join(output_folder, os.path.splitext(input_file)[0]+"_upsc.wav")
+      finalupscoutput.export(output_file, format="wav")
 
 if __name__ == '__main__':
   # argparse short params
   parser = argparse.ArgumentParser()
-  parser.add_argument('-i', '--input', help='Input File', required=True)
-  parser.add_argument('-o', '--output', help='Output File', required=True)
-  parser.add_argument('-c', '--ckpt', help='Checkpoint Path', required=False, default='rpsmai_diffusion_musdb18hq_ins_v1.ckpt')
+  parser.add_argument('-i', '--input', help='Input Folder', required=True)
+  parser.add_argument('-o', '--output', help='Output Folder', required=True)
+  parser.add_argument('-c', '--ckpt', help='Checkpoint Path', required=False, default='rpsmai_diffusion_23pop_ins_v1.ckpt')
   parser.add_argument('-s', '--sampler', help='Sampler', required=False, default='adaptive')
   parser.add_argument('-t', '--steps', help='Steps', required=False, default='12')
   parser.add_argument('-n', '--noise', help='Noise Level', required=False, default='0.6')
